9f1d2680eac6b2d619a97d3407de708cb5125821
1158558425 <1610130044@vip.henu.edu.cn>
2018-05-15 15:25:46 +0800

    算法11

---
+++ 算法11/算法11/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace 算法11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.DrawRect = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(49, 499);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "画直线";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DrawRect
+            // 
+            this.DrawRect.Location = new System.Drawing.Point(168, 499);
+            this.DrawRect.Name = "DrawRect";
+            this.DrawRect.Size = new System.Drawing.Size(75, 23);
+            this.DrawRect.TabIndex = 1;
+            this.DrawRect.Text = "画矩形";
+            this.DrawRect.UseVisualStyleBackColor = true;
+            this.DrawRect.Click += new System.EventHandler(this.DrawRect_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "画椭圆";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(402, 499);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "画任意多边形";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 589);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DrawRect);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "画图";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DrawRect;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 算法11/算法11/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 算法11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();//定义一个图像
+            g.DrawLine(Pens.Blue, 100.5F, 100.5F, 200.5F, 200.5F);
+            g.DrawLine(Pens.Red, 200.5F, 200.5F, 500.5F, 200.5F);
+            g.DrawLine(Pens.Purple, new Point(200, 200), new Point(300, 300));
+
+            Pen RedPen = new Pen(Color.Plum, 5);
+            g.DrawLine(RedPen, 400.5F, 100.5F, 200.5F, 200.5F);
+
+            g.Clear(Color.Gray);
+            Pen Green = new Pen(Color.Green, 5);
+            Point p1 = new Point(100, 500);
+            Point p2 = new Point(10, 50);
+            g.DrawLine(Green, p1, p2);
+            //Pen p1 = new(Brush)
+
+
+
+        }
+
+        private void DrawRect_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen RedPen = new Pen(Color.Red, 5);
+            g.Clear(Color.Azure);
+            g.DrawRectangle(RedPen, 0, 0, 300, 500);
+            Pen bluepen = new Pen(Color.Blue, 1);
+            g.DrawRectangle(bluepen, 3.5F,3.5F, 300.1F, 500.2F);
+            Pen yellowPen = new Pen(Color.Yellow, 5);
+            Rectangle R = new Rectangle(10, 10, 300, 500);
+            g.DrawRectangle(bluepen, R);
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen RedPen = new Pen(Color.Red, 2);
+            g.Clear(Color.Azure);//用颜色清理屏幕
+            g.DrawEllipse(RedPen, 200, 200, 500, 200);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen RedPen = new Pen(Color.Red, 2);
+            g.Clear(Color.Azure);
+            PointF p1 = new PointF(200, 200);
+            PointF p2 = new PointF(300, 200);
+            PointF p3 = new PointF(200, 300);
+            PointF p4 = new PointF(250, 350);
+            PointF p5 = new PointF(300, 300);
+            PointF[] points = { p1, p2, p3, p4, p5 };
+            g.DrawPolygon(RedPen,points);
+        }
+    }
+}
